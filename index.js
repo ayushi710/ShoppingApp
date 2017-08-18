95ea06ea7ca0850513a5b4e5ed4bad3f90db136c
ayushi <ayushimittal710@gmail.com>
2017-08-19 01:08:33 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ayushimittal on 18/08/17.
+ */
+var app= angular.module("myApp",[]);
+app.controller("myCtrl",function ($scope) {
+ $scope.items =["Milk","Bread","Cheese"];
+ $scope.add = function (newItem) {
+     $scope.items.push(newItem);
+     $scope.newItem="";
+ };
+ $scope.deleteItem = function (item) {
+     for (var i = 0; i<$scope.items.length ;i++)
+     {
+         if($scope.items[i] == item)
+         {
+             $scope.items.splice(i,1);
+             break;
+         }
+     }
+
+ };
+});
